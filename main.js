adacc180b71458d2f90b04172f2571df790ced74
rmkeezer <rmkeezer@yahoo.com>
2017-07-09 02:50:27 -0500

    Added magpie logo

--- main.js
+++ main.js
@@ -11,16 +11,16 @@ var refreshSortable = function() {
 }
 refreshSortable();
 
-var formatCards = function(data, num) {
+var formatCards = function(data) {
     out = '<ul class="col-lg-6 connectedSortable">';
     items = data.Items;
-    for (var i=0; i<num; i++) {
+    for (var i=0; i<items.length; i++) {
         start = '<div class="box">';
         icon = '<img class="card-image" src="' + items[i][8] + '">';
         //icon = '<img src="' + items[i][8] + '">'
         mask = '<div class="card-mask"></div>'
         body = '<div class="card-content box-header">'
-            + '<a class="card-title" href="#">' + items[i][1] + '</a>'
+            + '<a class="card-title" href="https://store.steampowered.com/app/' + items[i][0] +'">' + items[i][1] + '</a>'
             + '<div class="card-text">' + items[i][18] + '</div>'
             + '</div>';
         if (items[i][16] == '0') {
@@ -39,6 +39,7 @@ var formatCards = function(data, num) {
 
 $("#Dashboard").click(function(e) {
     $("#Dashboard").addClass('active');
+    $("#Library").removeClass('active');
     $("#Games").removeClass('active');
     $("#Discover").removeClass('active');
 
@@ -47,19 +48,57 @@ $("#Dashboard").click(function(e) {
     $("#content").html("");
 });
 
+$("#Library").click(function(e) {
+    $("#Dashboard").removeClass('active');
+    $("#Library").addClass('active');
+    $("#Games").removeClass('active');
+    $("#Discover").removeClass('active');
+
+    $("#Title").text("Library");
+
+    $.getJSON('http://97.79.174.132:5000/GetJoinedRowsOrdered', {
+        email: 'rmkeezer@yahoo.com',
+        password: 'smoothie42',
+        table1: 'usergames',
+        table2: 'games',
+        numRows: '10',
+        order: 'Id',
+        dir: 'DESC'
+    }, function(data) {
+        out = formatCards(data);
+        $("#content").html(out);
+        refreshSortable();
+    });
+
+    $.getJSON('http://97.79.174.132:5000/GetRowsOrdered', {
+        email: 'rmkeezer@yahoo.com',
+        password: 'smoothie42',
+        tableName: 'games',
+        numRows: '10',
+        order: 'Id',
+        dir: 'DESC'
+    }, function(data) {
+        out = formatCards(data);
+        $("#content").html($("#content").html() + out);
+        refreshSortable();
+    });
+});
+
 $("#Games").click(function(e) {
     $("#Dashboard").removeClass('active');
+    $("#Library").removeClass('active');
     $("#Games").addClass('active');
     $("#Discover").removeClass('active');
 
     $("#Title").text("Games");
 
-    $.getJSON('http://97.79.174.132:5000/GetAllRows', {
+    $.getJSON('http://97.79.174.132:5000/GetRows', {
         email: 'rmkeezer@yahoo.com',
         password: 'smoothie42',
-        tableName: 'games'
+        tableName: 'games',
+        numRows: '10'
     }, function(data) {
-        out = formatCards(data, 10);
+        out = formatCards(data);
         $("#content").html(out);
         refreshSortable();
     });
@@ -67,6 +106,7 @@ $("#Games").click(function(e) {
 
 $("#Discover").click(function(e) {
     $("#Dashboard").removeClass('active');
+    $("#Library").removeClass('active');
     $("#Games").removeClass('active');
     $("#Discover").addClass('active');
 
@@ -78,7 +118,7 @@ $("#Discover").click(function(e) {
         tableName: 'games',
         numRows: '10'
     }, function(data) {
-        out = formatCards(data, 10);
+        out = formatCards(data);
         $("#content").html(out);
         refreshSortable();
     });
